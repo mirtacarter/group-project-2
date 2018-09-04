46f3104db8b063e94e096c0ae3a2689de0cd7d77
Dalton Duncan <daltonduncan44@gmail.com>
2018-09-04 10:28:06 -0400

    linkedin api and table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,41 @@
 var db = require("../models");
 
-module.exports = function(app) {
+//module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+ // app.get("/api/examples", function(req, res) {
+   // db.Example.findAll({}).then(function(dbExamples) {
+//      res.json(dbExamples);
+//    });
+//  });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+//  app.post("/api/examples", function(req, res) {
+//    db.Example.create(req.body).then(function(dbExample) {
+//      res.json(dbExample);
+//    });
+//  });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //app.delete("/api/examples/:id", function(req, res) {
+    //db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+      //res.json(dbExample);
+    //});
+ // });
+//};
+
+
+
+  // POST route for saving a new post
+  // req.body is determined by api 
+  app.post("/api/jobs", function(req, res) {
+    console.log(req.body);
+    db.Jobs.create({
+      companyName: req.body.companyName,
+      location: req.body.location,
+      jobTitle: req.body.jobTitle,
+      link:req.body.link
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
   });
-};
